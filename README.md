2f64fccbdf211d2cbc99e4f5af1d7e2da6c82975
leunamzerep <manuelsph.315@gmail.com>
2025-08-24 03:00:31 -0500

    clone repo

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # game_theory
+Web app to collect info from a negotiation game to an economic study
---
+++ game.js
@@ -0,0 +1,114 @@
+$(document).ready(function () {
+    var step = 1;
+    let error = false;
+    var percievedValueBySeller;
+    var topPriceBySeller;
+    var proposedPriceBySeller;
+    var percievedValueByBuyer;
+    var topPriceByBuyer;
+    var proposedPriceByBuyer;
+    var averagePrice;
+
+    $(".next").on("click", function () {
+        error = false;
+        if (step === 4 || step === 8) {
+
+            let prefix = (step === 4) ? "Seller" : "Buyer";
+
+            let countPercieved = $("#percievedValueBy" + prefix).val().replace(/\D/g, "").length;
+            let countTop = $("#topPriceBy" + prefix).val().replace(/\D/g, "").length;
+            let countProposed = $("#proposedPriceBy" + prefix).val().replace(/\D/g, "").length;
+
+            if (countPercieved < 6) {
+                error = true;
+                $("#percievedValueBy" + prefix).next().show();
+            } else {
+                $("#percievedValueBy" + prefix).next().hide();
+            }
+            if (countTop < 6) {
+                error = true;
+                $("#topPriceBy" + prefix).next().show();
+            } else {
+                $("#topPriceBy" + prefix).next().hide();
+            }
+            if (countProposed < 6) {
+                error = true;
+                $("#proposedPriceBy" + prefix).next().show();
+            } else {
+                $("#proposedPriceBy" + prefix).next().hide();
+            }
+        }
+
+        if (error) {
+            alert("Por favor, complete todos los campos con 6 o 7 dígitos.");
+            return;
+        } else {
+            error = false;
+            if (step == 8) {
+                percievedValueBySeller = parseInt($("#percievedValueBySeller").val().replace(/\D/g, ""));
+                topPriceBySeller = parseInt($("#topPriceBySeller").val().replace(/\D/g, ""));
+                proposedPriceBySeller = parseInt($("#proposedPriceBySeller").val().replace(/\D/g, ""));
+                percievedValueByBuyer = parseInt($("#percievedValueByBuyer").val().replace(/\D/g, ""));
+                topPriceByBuyer = parseInt($("#topPriceByBuyer").val().replace(/\D/g, ""));
+                proposedPriceByBuyer = parseInt($("#proposedPriceByBuyer").val().replace(/\D/g, ""));
+                averagePrice = (topPriceBySeller + proposedPriceBySeller + topPriceByBuyer + proposedPriceByBuyer) / 4;
+
+                let prices = [
+                    averagePrice,
+                    topPriceBySeller,
+                    proposedPriceBySeller,
+                    topPriceByBuyer,
+                    proposedPriceByBuyer
+                ];
+
+                prices.sort((a, b) => a - b);
+
+                prices.forEach((price, index) => {
+                    let formatted = price.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ".");
+                    $("#price" + (index + 1)).text(formatted + "$");
+                });
+            };
+            $('#step' + step).hide();
+            step++;
+            $('#step' + step).show();
+        }
+    });
+
+    $(" .prices").on("click", function () {
+        $('#step' + step).hide();
+        step++;
+        $('#step' + step).show();
+    });
+
+    $(".moneyInput").on("focus", function () {
+        if ($(this).val() === "$") {
+            setCaret(this, 0);
+        }
+    });
+
+    $(".moneyInput").on("input", function () {
+        let val = $(this).val().replace(/\D/g, "");
+        if (val.startsWith("0")) {
+            val = val.replace(/^0+/, "");
+        }
+        val = val.slice(0, 7);
+        let formatted = val.replace(/\B(?=(\d{3})+(?!\d))/g, ".");
+        $(this).val(formatted + " $");
+        setCaret(this, formatted.length);
+    });
+
+    function setCaret(el, pos) {
+        requestAnimationFrame(() => {
+            el.setSelectionRange(pos, pos);
+        });
+    }
+
+    $(".copyLink").on("click", function () {
+        let url = window.location.href;
+        navigator.clipboard.writeText(url).then(() => {
+            alert("¡Link copiado al portapapeles!");
+        }).catch(err => {
+            console.error("Error al copiar: ", err);
+        });
+    });
+});
